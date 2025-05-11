dfda9a62a56f4c15a8307adb707b5f47571cce67
Martin Collins <martin_collins19@yahoo.com>
2025-05-11 06:28:46 -0500

    added new cw

---
+++ 8KYU-is-str-uppercase.js
@@ -0,0 +1,16 @@
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+
+// My solution
+
+String.prototype.isUpperCase = function() {
+    return this.toUpperCase() === this.toString();
+  }
